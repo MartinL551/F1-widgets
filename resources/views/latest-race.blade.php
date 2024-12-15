5c1144b7edf91f7183f789bc19e9045441929086
MartinL551 <levermartin555@gmail.com>
2024-12-15 15:37:27 +0000

    new: added in first controller, grabbed info from open api

---
+++ resources/views/latest-race.blade.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1>{{ $jsonString }} </h1>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\calendarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/calendar', [calendarController::class, 'showLatestRace']);
