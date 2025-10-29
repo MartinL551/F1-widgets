504df813a24e768cc35ac86ffbe9d549c2b3925f
home <levermartin555@gmail.com>
2025-10-29 20:37:49 +0000

    update: first try of cache logic

--- app/Service/OpenF1.php
+++ app/Service/OpenF1.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use App\Models\F1Race;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Uri;
+use Illuminate\Support\Facades\Cache;
 
 class OpenF1
 {
@@ -35,8 +36,21 @@ class OpenF1
     {
         $currentRace = F1Race::getLatestRace();
         $sessionKey = $this->getLatestSessionType();
+
+
+
+        if (($cached = Cache::get('race_control')) && $cached['meeting_key'] === $currentRace->race_api_key) {
+            if ($this->checkCacheExpired($cached['timestamp'] ?? null)) {
+                Cache::forget('race_control');
+            } else {
+                return $cached['messages'];
+            }
+        }
+
         $url = (string) $this->api_base_url->withPath('v1/race_control')->withQuery(['meeting_key' => $currentRace->race_api_key, 'session_key' => $sessionKey]);
         $data = Http::get($url);
+
+        Cache::set('race_control', ['messages' => $data->json(), 'meeting_key' => $currentRace->race_api_key, 'timestamp' => now()]);
 
         return $data->json();
     }
@@ -44,10 +58,36 @@ class OpenF1
     public function getLatestSessionType()
     {
         $currentRace = F1Race::getLatestRace();
+
+        if (($cached = Cache::get('session')) && $cached['meeting_key'] === $currentRace->race_api_key) {
+            if ($this->checkCacheExpired($cached['timestamp'] ?? null)) {
+                Cache::forget('session');
+            } else {
+                return $cached['session_key'];
+            }
+        }
+
         $url = (string) $this->api_base_url->withPath('v1/sessions')->withQuery(['meeting_key' => $currentRace->race_api_key, 'session_key' => 'latest']);
         $data = Http::get($url);
         $data = $data->json();
 
+        Cache::set('session', ['session_key' => $data[0]['session_key'],'meeting_key' => $currentRace->race_api_key, 'timestamp' => now()]);
+
         return $data[0]['session_key'];
+    }
+
+    public function expireCurrentCache()
+    {
+        Cache::forget('session');
+        Cache::forget('race_control');
+    }
+
+    public function checkCacheExpired($cacheTimestamp)
+    {
+        $now = now();
+        $cacheTime = \Carbon\Carbon::parse($cacheTimestamp);
+        $diffInHours = $now->diffInHours($cacheTime);
+
+        return $diffInHours >= 12;
     }
 }
